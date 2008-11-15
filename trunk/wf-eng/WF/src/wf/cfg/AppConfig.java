e2f130da4bfdb691098e34db2a8230b774d99ee0
sergiunix <sergiunix@a88d6a4a-b03f-11dd-9763-1166fc48c086>
2008-11-15 08:26:05 +0000

    bug fixes :)

--- trunk/wf-eng/WF/src/wf/cfg/AppConfig.java
+++ trunk/wf-eng/WF/src/wf/cfg/AppConfig.java
@@ -1,4 +1,3 @@
-
 package wf.cfg;
 
 import org.apache.log4j.Logger;
@@ -9,88 +8,98 @@ import java.io.InputStream;
 
 public class AppConfig {
 
-  public static final String XFLOW_CONFIG = "app.properties";
+	public static final String configFile = "app.properties";
 
-  private static Logger log = Logger.getLogger(AppConfig.class);
+	private static Logger log = Logger.getLogger(AppConfig.class);
 
-  private static AppConfig _xXflowConfig;
+	private static AppConfig appConfig;
 
-  private Properties config = new Properties();
+	private Properties config = new Properties();
 
-  private AppConfig(){
-    try{
-      URL url = this.getClass().getClassLoader().getResource( XFLOW_CONFIG );
-      if( url == null ){
-        url = Thread.currentThread().getContextClassLoader().getResource( XFLOW_CONFIG );
-      }
-      if( url != null ){
-        InputStream inputStream = url.openStream();
-        config.load( inputStream );
-        inputStream.close();
-      }
-    } catch( Exception e){
-      e.printStackTrace();
-    }
+	private AppConfig() {
+		try {
+			URL url = this.getClass().getClassLoader()
+					.getResource(configFile);
+			if (url == null) {
+				url = Thread.currentThread().getContextClassLoader()
+						.getResource(configFile);
+			}
+			if (url != null) {
+				InputStream inputStream = url.openStream();
+				config.load(inputStream);
+				inputStream.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-  }
+	}
 
-  public synchronized static AppConfig getInstance(){
-    if( _xXflowConfig == null ){
-      _xXflowConfig = new AppConfig();
-    }
-    return _xXflowConfig;
-  }
+	public synchronized static AppConfig getInstance() {
+		if (appConfig == null) {
+			appConfig = new AppConfig();
+		}
+		return appConfig;
+	}
 
+	public String get(String key, String def) {
+		String v = (String) config.get(key);
+		if (v == null) {
+			log.info("key " + key + " was not found in " + configFile
+					+ ". Will use default value [" + def + "]");
+			put(key, def);
+			return def;
+		}
+		return v;
+	}
 
-  public String  get(String key,String def) {
-    String v = (String) config.get( key );
-    if( v == null ) {
-      log.info( "key " + key + " was not found in " + XFLOW_CONFIG + ". Will use default value [" + def +"]" );
-      put( key, def );
-      return def;
-    }
-    return v;
-  }
+	public int getInt(String key, int i) {
+		String v = (String) config.get(key);
+		if (v == null) {
+			log.info("key " + key + " was not found in " + configFile
+					+ ". Will use default value [" + i + "]");
+			putInt(key, i);
+			return i;
+		}
+		return Integer.parseInt(v);
+	}
 
-  public int getInt(String key, int i) {
-    String v = (String) config.get( key );
-    if( v == null ) {
-      log.info( "key " + key + " was not found in " + XFLOW_CONFIG + ". Will use default value [" + i +"]" );
-      putInt( key, i );
-      return i;
-    }
-    return Integer.parseInt( v );
-  }
+	public void put(String key, String val) {
+		config.put(key, val);
+	}
 
-  public void put( String key, String val ){
-    config.put( key, val );
-  }
+	public void putInt(String key, int i) {
+		config.put(key, String.valueOf(i));
+	}
 
-  public void putInt(String key, int i) {
-    config.put( key, String.valueOf( i ) );
-  }
-  public static final	String XFLOW_CONNECTION_FACTORY(){
-    return getInstance().get( "XFLOW_CONNECTION_FACTORY",  "ConnectionFactory" );
-  }
+	public static final String getConnectionFactory() {
+		return getInstance().get("WF_CONNECTIONFACTORY",
+				"ConnectionFactory");
+	}
 
-  public static final String XFLOW_TOPIC(){
-    return getInstance().get( "XFLOW_TOPIC", "topic/inboxTopic");
-  }
-  public static final String XFLOW_EVENT_TOPIC(){
-    return getInstance().get( "XFLOW_EVENT_TOPIC", "topic/XFLOW.EVENT");
-  }
-  public static final String XFLOW_QUEUE(){
-    return getInstance().get( "XFLOW_QUEUE", "queue/xflowReplyQueue");
-  }
-  public static final	String WORKFLOWENGINE_QUEUE(){
-    return getInstance().get("WORKFLOWENGINE_QUEUE", "queue/WorkflowEngineQueue");
-  }
-  public static final	String XFLOW_DATASOURCE(){
-    return getInstance().get("XFLOW_DATASOURCE", "java:/XflowDS");
-  }
+	public static final String getInboxTopic() {
+		return getInstance().get("WFTOPIC_INBOX", "topic/WFTOPIC_INBOX");
+	}
 
-  public static String DB_CREATE_SCRIPT() {
-    return getInstance().get( "CREATEDB-SQL","conf/create_db.sql");
-  }
-  
+	public static final String getOutboxQueue() {
+		return getInstance().get("WFQUEUE_OUTBOX", "queue/WFQUEUE_OUTBOX");
+	}
+
+	public static final String getEventsTopic() {
+		return getInstance().get("WFTOPIC_EVENTS", "topic/WFTOPIC_EVENTS");
+	}
+
+	public static final String getWfQueue() {
+		return getInstance()
+				.get("WFQUEUE_ENGINE", "queue/WFQUEUE_ENGINE");
+	}
+
+	public static final String getDataSource() {
+		return getInstance().get("WFDS", "java:/WFDS");
+	}
+
+	public static String DB_CREATE_SCRIPT() {
+		return getInstance().get("CREATEDB-SQL", "conf/create_db.sql");
+	}
+
 }
