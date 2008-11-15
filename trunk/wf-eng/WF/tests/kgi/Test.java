ea8eb2a7d32ccadcd39f7e38367a8e3f871fae1d
sergiunix <sergiunix@a88d6a4a-b03f-11dd-9763-1166fc48c086>
2008-11-15 08:32:37 +0000

    bug fixes :)

--- trunk/wf-eng/WF/tests/kgi/Test.java
+++ trunk/wf-eng/WF/tests/kgi/Test.java
@@ -1,34 +1,29 @@
 package kgi;
 
-
 public class Test {
 
-  public static class T2 extends Thread{
-    public void run() {
-      for( int i = 0; i < 200; i++){
-        System.out.println("i=" + i);
-      }
-    }
+	public static class T2 extends Thread {
+		public void run() {
+			for (int i = 0; i < 200; i++) {
+				System.out.println("i=" + i);
+			}
+		}
 
-  }
+	}
 
-  public class T3 extends Thread{
-    public void run() {
-      for( int i = 0; i < 200; i++){
-        System.out.println("i=" + i);
-      }
-    }
+	public class T3 extends Thread {
+		public void run() {
+			for (int i = 0; i < 200; i++) {
+				System.out.println("i=" + i);
+			}
+		}
 
-  }
+	}
 
+	public static void main(String[] args) {
+		T2 t2 = new T2();
+		t2.start();
 
-  public static void main(String[] args) {
-    T2 t2 = new T2();
-    t2.start();
-
-    
-  }
+	}
 
 }
-
-
