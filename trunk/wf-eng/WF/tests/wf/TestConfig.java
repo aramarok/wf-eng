8ac1f1820ac784b8e0a43f4d0609a3bb59805c72
dreytc <dreytc@a88d6a4a-b03f-11dd-9763-1166fc48c086>
2008-12-15 08:09:03 +0000

    benzinarie - test case example

--- trunk/wf-eng/WF/tests/wf/TestConfig.java
+++ trunk/wf-eng/WF/tests/wf/TestConfig.java
@@ -4,7 +4,7 @@ import wf.client.auth.User;
 
 public class TestConfig {
 
-	private static User user = new User("user", "password");
+	private static User user = new User("test_user", "test_password");
 
 	public static User getUser() {
 		return user;
