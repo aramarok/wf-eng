f80eba8696de30a515345404094e3e97d8907e12
ivascaflavius <ivascaflavius@a88d6a4a-b03f-11dd-9763-1166fc48c086>
2009-03-07 18:29:21 +0000

    Ctrl + Shift + O

--- trunk/wf-eng/WF_GUI_Swing/src/com/uvt/wf/designer/gui/editor/WorkflowDesigner.java
+++ trunk/wf-eng/WF_GUI_Swing/src/com/uvt/wf/designer/gui/editor/WorkflowDesigner.java
@@ -26,7 +26,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
